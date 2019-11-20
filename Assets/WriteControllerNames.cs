d20c64c6bd11859c6561b17277604eb168a8a3bf
Kurtis <kurtie@microsoft.com>
2019-11-19 17:44:06 -0800

    stash

---
+++ Assets/WriteControllerNames.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class WriteControllerNames : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro text = null;
+
+    private void Update()
+    {
+        InputDevice left = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        InputDevice right = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        text.text = $"{left.name} | {left.manufacturer} | {left.serialNumber}\n";
+        text.text += $"{right.name} | {right.manufacturer} | {right.serialNumber}\n";
+    }
+}
