71fe4de639759568d49fc512ada1a27ca71f5903
Kurtis <kurtie@microsoft.com>
2020-01-15 12:07:44 -0800

    Move VersionDefine into its own file

--- Assets/MRTK/Core/Utilities/Editor/AssemblyDefinition.cs
+++ Assets/MRTK/Core/Utilities/Editor/AssemblyDefinition.cs
@@ -258,36 +258,4 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Editor
             References = refs.ToArray();
         }
     }
-
-#if UNITY_2019_3_OR_NEWER
-    /// <summary>
-    /// Represents a subclass of a Unity assembly definition (asmdef) file.
-    /// </summary>
-    [Serializable]
-    public struct VersionDefine : IEquatable<VersionDefine>
-    {
-        public VersionDefine(string name, string expression, string define)
-        {
-            this.name = name;
-            this.expression = expression;
-            this.define = define;
-        }
-
-        [SerializeField]
-        private string name;
-
-        [SerializeField]
-        private string expression;
-
-        [SerializeField]
-        private string define;
-
-        bool IEquatable<VersionDefine>.Equals(VersionDefine other)
-        {
-            return name.Equals(other.name) &&
-                expression.Equals(other.expression) &&
-                define.Equals(other.define);
-        }
-    }
-#endif // UNITY_2019_3_OR_NEWER
 }
