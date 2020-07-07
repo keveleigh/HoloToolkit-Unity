30b3f6d3be04ccde95eb10a6bc6ffcdf33a471dd
Kurtis Eveleigh <kurtis.eveleigh@gmail.com>
2020-07-07 10:40:10 -0700

    stash

---
+++ Assets/MRTK/Core/Interfaces/InputSystem/IMixedRealityControllerMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.MixedReality.Toolkit.Input
+{
+    public interface IMixedRealityControllerMappingProfile
+    {
+        /// <summary>
+        /// The list of controller mappings your application can use.
+        /// </summary>
+        MixedRealityControllerMapping[] MixedRealityControllerMappings { get; }
+    }
+}
